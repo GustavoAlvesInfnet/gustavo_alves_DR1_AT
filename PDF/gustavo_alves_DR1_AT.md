0c3bff755c6867ba738e4692a033e1d95273da46
“dsCarneiro03@gmail.com” <dscarneiro03@gmail.com>
2024-10-01 22:32:14 -0300

    Ralização de tudo até o 8

--- PDF/gustavo_alves_DR1_AT.md
+++
@@ -1,3 +0,0 @@
-# Preparar o ambiente
-![alt text](image.png)
-
--- main/football_analysis.py
+++ main/football_analysis.py
@@ -3,6 +3,9 @@ from statsbombpy import sb
 from mplsoccer import Pitch, Sbopen
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
+import spinners
+import time
 
 parser = Sbopen()
 
@@ -25,8 +28,32 @@ def plot_passes(match, player_name):
 
     return fig
 
+def plot_passes_team(match, team):
+    player_filter = (match.type_name=="Pass") & (match.team_name==team)
+    df_pass = match.loc[player_filter, ['x', 'y', 'end_x', 'end_y']]
+
+    pitch = Pitch(line_color="black", pitch_color="#799351", stripe_color="#799351", stripe=True)
+    fig, ax = pitch.grid(grid_height=0.9, title_height=0.06, endnote_height=0.04, axis=False, title_space=0, endnote_space=0)
+
+    pitch.arrows(df_pass.x, df_pass.y, df_pass.end_x, df_pass.end_y, width=2, color="white", ax=ax["pitch"])
+    pitch.kdeplot(x=df_pass.x, y=df_pass.y, ax=ax["pitch"], shade=True, alpha=0.5, cmap="plasma")
+
+    return fig
+
 def plot_shots(match, player_name):
     player_filter = (match.type_name=="Shot") & (match.player_name==player_name)
+    df_pass = match.loc[player_filter, ['x', 'y', 'end_x', 'end_y']]
+
+    pitch = Pitch(line_color="black", pitch_color="#799351", stripe_color="#799351", stripe=True)
+    fig, ax = pitch.grid(grid_height=0.9, title_height=0.06, endnote_height=0.04, axis=False, title_space=0, endnote_space=0)
+
+    pitch.arrows(df_pass.x, df_pass.y, df_pass.end_x, df_pass.end_y, width=2, color="white", ax=ax["pitch"])
+    pitch.kdeplot(x=df_pass.x, y=df_pass.y, ax=ax["pitch"], shade=True, alpha=0.5, cmap="plasma")
+
+    return fig
+
+def plot_shots_team(match, team):
+    player_filter = (match.type_name=="Shot") & (match.team_name==team)
     df_pass = match.loc[player_filter, ['x', 'y', 'end_x', 'end_y']]
 
     pitch = Pitch(line_color="black", pitch_color="#799351", stripe_color="#799351", stripe=True)
@@ -89,6 +116,8 @@ def plot_conversion(matches, game, home_team, away_team):
             return(fig)
 
 def main():
+
+
     st.title("Gustavo Alves DR1 AT - Análises de Futebol")
 
     menu = ["Home", "Dados por competição", "Dados por jogador", "Análises"]
@@ -98,13 +127,14 @@ def main():
         st.subheader("Home")
 
     elif choice == "Dados por competição":
+        
         st.subheader("Explore os dados sobre as partidas e torneios")
 
         competitions = sb.competitions()
         competitions_names = competitions["competition_name"].unique()
         competition = st.selectbox("Selecione a competição", competitions_names)
         competition_id = competitions[competitions["competition_name"] == competition]["competition_id"].values[0]
-        
+
         seasons = competitions[competitions["competition_name"] == competition]["season_name"].unique()
         season_name = st.selectbox("Selecione a Temporada", seasons)
         season_id = competitions[competitions["season_name"] == season_name]["season_id"].values[0]
@@ -112,64 +142,111 @@ def main():
 
         game = st.selectbox("Selecione a partida", matches["match_id"], format_func=lambda idx:get_match_label(matches, idx))
 
-        
+        # Botões de download
+        data = sb.events(match_id=game)
+        data.to_csv("dados_partida.csv", index=False)
+
+        with open("dados_partida.csv", "rb") as f:
+            dataDownload = f.read()
+
+        st.download_button(
+            label="Download dos dados da partida",
+            data=dataDownload,
+            file_name="dados_partida.csv",
+            mime="text/csv",
+        )
+
+
+            
         tab1, tab2, tab3 = st.tabs(["Informações sobre a partida", "Informações sobre os jogadores", "Tabelas"])
 
         with tab1:
+
+            st.markdown(f"<font size='6'>Informações sobre a partida <font color='#F63366'>{get_match_label(matches, game)}</font> na competição <font color='#F63366'>{competition}</font> na Temporada <font color='#F63366'>{season_name}</font></font>", unsafe_allow_html=True)
+
+
             with st.container():
-                st.markdown(f"<font size='6'>Informações sobre a partida <font color='#F63366'>{get_match_label(matches, game)}</font> na competição <font color='#F63366'>{competition}</font> na Temporada <font color='#F63366'>{season_name}</font></font>", unsafe_allow_html=True)
-
-
-
-            left_column, right_column = st.columns(2)
-            with st.container():
+                left_column, right_column = st.columns(2)
                 with left_column:
-                    st.markdown("<div style='text-align: center; font-size: 32px; font-weight: bold'>Time da Casa</div>", unsafe_allow_html=True)
+                    st.markdown("<div style='text-align: left; font-size: 32px; font-weight: bold'>Time da Casa</div>", unsafe_allow_html=True)
                     home_team = matches[matches["match_id"] == game]["home_team"].values[0]
-                    st.markdown(f"<div style='text-align: center; font-size: 24px; color: #F63366'>{home_team}</div>", unsafe_allow_html=True)
+                    st.markdown(f"<div style='text-align: left; font-size: 24px; color: #F63366'>{home_team}</div>", unsafe_allow_html=True)
                     home_score = matches[matches["match_id"] == game]["home_score"].values[0]
-                    st.markdown(f"<div style='text-align: center; font-size: 18px'>Gols: {home_score}</div>", unsafe_allow_html=True)
+                    #st.metric("Gols", home_score, delta_color="normal")
+                    st.markdown(f"<div style='font-size: 16px'>Gols:</div>", unsafe_allow_html=True)
+                    st.markdown(f"<div style='font-size: 36px; color: #F63366'>{home_score}</div>", unsafe_allow_html=True)
+
 
                     passes = sb.events(match_id=game, split=True, flatten_attrs=False)["passes"]
                     passes_home_team = passes["possession_team"] == home_team
                     passes_home_team=passes_home_team.sum()
-                    st.markdown(f"<div style='text-align: center; font-size: 18px'>Passes: {passes_home_team}</div>", unsafe_allow_html=True)
+                    st.metric("Passes", passes_home_team)
+                    #st.markdown(f"<div style='text-align: center; font-size: 18px'>Passes: {passes_home_team}</div>", unsafe_allow_html=True)
 
                     chutes = sb.events(match_id=game, split=True, flatten_attrs=False)["shots"]
                     chutes_home_team = chutes["possession_team"] == home_team
                     chutes_home_team=chutes_home_team.sum()
-                    st.markdown(f"<div style='text-align: center; font-size: 18px'>Chutes: {chutes_home_team}</div>", unsafe_allow_html=True)
+                    st.metric("Chutes", chutes_home_team)
+                    #st.markdown(f"<div style='text-align: center; font-size: 18px'>Chutes: {chutes_home_team}</div>", unsafe_allow_html=True)
+
 
 
                 with right_column:
-                    st.markdown("<div style='text-align: center; font-size: 32px; font-weight: bold'>Time Visitante</div>", unsafe_allow_html=True)
+                    st.markdown("<div style='text-align: left; font-size: 32px; font-weight: bold'>Time Visitante</div>", unsafe_allow_html=True)
                     away_team = matches[matches["match_id"] == game]["away_team"].values[0]
-                    st.markdown(f"<div style='text-align: center; font-size: 24px; color: #F63366'>{away_team}</div>", unsafe_allow_html=True)
+                    st.markdown(f"<div style='text-align: left; font-size: 24px; color: #F63366'>{away_team}</div>", unsafe_allow_html=True)
                     away_score = matches[matches["match_id"] == game]["away_score"].values[0]
-                    st.markdown(f"<div style='text-align: center; font-size: 18px'>Gols: {away_score}</div>", unsafe_allow_html=True)
+                    st.markdown(f"<div style='font-size: 18px'> Gols: </div>", unsafe_allow_html=True)
+                    st.markdown(f"<div style='font-size: 36px; color: #F63366'>{away_score}</div>", unsafe_allow_html=True)
+                    #st.metric(label="Gols", value=away_score)
 
                     passes = sb.events(match_id=game, split=True, flatten_attrs=False)["passes"]
                     passes_away_team = passes["possession_team"] == away_team
                     passes_away_team=passes_away_team.sum()
-                    st.markdown(f"<div style='text-align: center; font-size: 18px'>Passes: {passes_away_team}</div>", unsafe_allow_html=True)
-
+                    #st.markdown(f"<div style='text-align: center; font-size: 18px'>Passes: {passes_away_team}</div>", unsafe_allow_html=True)
+                    st.metric(label="Passes", value=passes_away_team)
 
                     chutes = sb.events(match_id=game, split=True, flatten_attrs=False)["shots"]
                     chutes_away_team = chutes["possession_team"] == away_team
                     chutes_away_team=chutes_away_team.sum()
-                    st.markdown(f"<div style='text-align: center; font-size: 18px'>Chutes: {chutes_away_team}</div>", unsafe_allow_html=True)
+                    #st.markdown(f"<div style='text-align: center; font-size: 18px'>Chutes: {chutes_away_team}</div>", unsafe_allow_html=True)
+                    st.metric(label="Chutes", value=chutes_away_team)
+                # arbitro
+                st.write("")
+                referee = matches[matches["match_id"] == game]["referee"].values[0]
+                st.markdown(f"<h6 style='text-align: center; color: white;'>Árbitro: {referee}</h6>", unsafe_allow_html=True)
+                st.write("")
 
+                partida = sb.matches(competition_id=competition_id, season_id=season_id)
+                final_match_id = partida[(partida["home_team"] == home_team) & (partida["away_team"] == away_team)].match_id.values[0]
+                final_data = match_data(final_match_id)
 
-            st.write("")
-            st.write("")
-            referee = matches[matches["match_id"] == game]["referee"].values[0]
-            st.markdown(f"<h6 style='text-align: center; color: white;'>Árbitro: {referee}</h6>", unsafe_allow_html=True)
+                with st.spinner("Página está carregando..."):
+                    st.subheader("Mapa de chutes")
+                    left_column, right_column = st.columns(2)
+                    with left_column:
+                        st.write(plot_shots_team(final_data, home_team))
+                    with right_column:
+                        st.write(plot_shots_team(final_data, away_team))
 
+                    st.subheader("Mapa de passes")
+                    left_column, right_column = st.columns(2)
+                    with left_column:
+                        st.write(plot_passes_team(final_data, home_team))
+                    with right_column:
+                        st.write(plot_passes_team(final_data, away_team))
+
+            st.title("Análises")    
             # Grafico de relação de chutes com gols
             st.write(plot_shots_x_goals(matches, game, home_team, away_team))
 
             # Grafico de % de conversao
             st.write(plot_conversion(matches, game, home_team, away_team))
+
+            # Eventos
+            st.title("Eventos")
+            events = sb.events(match_id=game)
+            st.write(events)
 
 
 
@@ -177,9 +254,10 @@ def main():
 
 
         with tab2:
-            st.markdown(f"<font size='6'>Eventos da partida <font color='#F63366'>{get_match_label(matches, game)}</font> na competição <font color='#F63366'>{competition}</font> na Temporada <font color='#F63366'>{season_name}</font></font>", unsafe_allow_html=True)
-            
+            st.markdown(f"<font size='6'>Informações sobre a partida <font color='#F63366'>{get_match_label(matches, game)}</font> na competição <font color='#F63366'>{competition}</font> na Temporada <font color='#F63366'>{season_name}</font></font>", unsafe_allow_html=True)
+
             st.title("Comparações individuais")
+
             # time da casa
             lineups = sb.lineups(match_id=game)
             jogadores = lineups[home_team].values
@@ -198,33 +276,68 @@ def main():
 
             jogador_visit = st.selectbox("Selecione um Jogador do time visitante", list_jogadores)
 
-            partida = sb.matches(competition_id=competition_id, season_id=season_id)
-            final_match_id = partida[(partida["home_team"] == home_team) & (partida["away_team"] == away_team)].match_id.values[0]
-            final_data = match_data(final_match_id)
-            col_1, col_2 = st.columns(2)
-
-            with col_1:
-                st.subheader(f"Passes de {jogador_casa.split()[0]}")
-                fig_1 = plot_passes(final_data, jogador_casa)
-                st.pyplot(fig_1)
-
-                st.subheader(f"Chutes de {jogador_casa.split()[0]}")
-                fig_1 = plot_shots(final_data, jogador_casa)
-                st.pyplot(fig_1)
+            # Intervalo de tempo
+            intervalo_tempo = st.slider("Selecione o intervalo de tempo", 0, 110, (0, 110))
 
 
-            with col_2:
-                st.subheader(f"Passes de {jogador_visit.split()[0]}")
-                fig_2 = plot_passes(final_data, jogador_visit)
-                st.pyplot(fig_2)
+            # Opções de estatísticas
+            estatisticas_opcoes = st.multiselect("Selecione as estatísticas que deseja ver", ["Passes", "Chutes"])
 
-                st.subheader(f"Chutes de {jogador_visit.split()[0]}")
-                fig_2 = plot_shots(final_data, jogador_visit)
-                st.pyplot(fig_2)
+
+            # Botão para atualizar os gráficos
+            atualizar_graficos = st.button("Atualizar gráficos")
+
+            if atualizar_graficos:
+                partida = sb.matches(competition_id=competition_id, season_id=season_id)
+                final_match_id = partida[(partida["home_team"] == home_team) & (partida["away_team"] == away_team)].match_id.values[0]
+                final_data = match_data(final_match_id)
+                col_1, col_2 = st.columns(2)
+
+                with col_1:
+                    if "Passes" in estatisticas_opcoes:
+                        st.subheader(f"Passes de {jogador_casa.split()[0]}")
+                        bar = st.progress(0)
+                        fig_1 = plot_passes(final_data, jogador_casa)
+                        for i in range(100):
+                            bar.progress(i + 1)
+                            time.sleep(0.01)
+                        bar.empty()  # Limpa a barra de progresso
+                        st.pyplot(fig_1)
+
+                    if "Chutes" in estatisticas_opcoes:
+                        st.subheader(f"Chutes de {jogador_casa.split()[0]}")
+                        bar = st.progress(0)
+                        fig_1 = plot_shots(final_data, jogador_casa)
+                        for i in range(100):
+                            bar.progress(i + 1)
+                            time.sleep(0.01)
+                        bar.empty()  # Limpa a barra de progresso
+                        st.pyplot(fig_1)
+
+                with col_2:
+                    if "Passes" in estatisticas_opcoes:
+                        st.subheader(f"Passes de {jogador_visit.split()[0]}")
+                        bar = st.progress(0)
+                        fig_2 = plot_passes(final_data, jogador_visit)
+                        for i in range(100):
+                            bar.progress(i + 1)
+                            time.sleep(0.01)
+                        bar.empty()  # Limpa a barra de progresso
+                        st.pyplot(fig_2)
+
+                    if "Chutes" in estatisticas_opcoes:
+                        st.subheader(f"Chutes de {jogador_visit.split()[0]}")
+                        bar = st.progress(0)
+                        fig_2 = plot_shots(final_data, jogador_visit)
+                        for i in range(100):
+                            bar.progress(i + 1)
+                            time.sleep(0.01)
+                        bar.empty()  # Limpa a barra de progresso
+                        st.pyplot(fig_2)
 
 
         with tab3:
-            st.markdown(f"<font size='6'>Eventos da partida <font color='#F63366'>{get_match_label(matches, game)}</font> na competição <font color='#F63366'>{competition}</font> na Temporada <font color='#F63366'>{season_name}</font></font>", unsafe_allow_html=True)
+            st.markdown(f"<font size='6'>Informações sobre a partida <font color='#F63366'>{get_match_label(matches, game)}</font> na competição <font color='#F63366'>{competition}</font> na Temporada <font color='#F63366'>{season_name}</font></font>", unsafe_allow_html=True)
             
             st.title("Eventos")
             events = sb.events(match_id=game)
@@ -249,14 +362,10 @@ def main():
             dribbles = sb.events(match_id=game, split=True, flatten_attrs=False)["dribbles"]
             st.dataframe(dribbles)
 
-            st.title("Faltas")
-            fouls = sb.events(match_id=game, split=True, flatten_attrs=False)["fouls"]
-            st.dataframe(fouls)
-
-            st.title("Cartões")
-            cards = sb.events(match_id=game, split=True, flatten_attrs=False)["cards"]
-            st.dataframe(cards)
-
+            st.title("Matches")
+            matches = sb.matches(competition_id=competition_id, season_id=season_id)
+            matches = matches[matches["match_id"] == game]
+            st.dataframe(matches)
             
             
 
